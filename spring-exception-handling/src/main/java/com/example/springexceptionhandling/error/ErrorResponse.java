9b7d9552cec069ce62426f17cb4cb570c054f388
rlvy98 <rlvy98@naver.com>
2023-08-07 16:10:58 +0900

    feat: ErrorCode 확장

--- spring-exception-handling/src/main/java/com/example/springexceptionhandling/error/ErrorResponse.java
+++ spring-exception-handling/src/main/java/com/example/springexceptionhandling/error/ErrorResponse.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ErrorResponse {
+    private String httpStatus;
     private String errorCode;
     private String errorMessage;
 
-    public static ErrorResponse of(String errorCode, String errorMessage) {
+    public static ErrorResponse of(String httpStatus, String errorCode, String errorMessage) {
         return ErrorResponse.builder()
+                .httpStatus(httpStatus)
                 .errorCode(errorCode)
                 .errorMessage(errorMessage)
                 .build();
--- spring-exception-handling/src/main/java/com/example/springexceptionhandling/error/GlobalExceptionHandler.java
+++ spring-exception-handling/src/main/java/com/example/springexceptionhandling/error/GlobalExceptionHandler.java
@@ -1,26 +1,44 @@
 package com.example.springexceptionhandling.error;
 
+import com.example.springexceptionhandling.error.exception.AuthenticationException;
 import com.example.springexceptionhandling.error.exception.TestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("Exception", e);
-        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-    }
 
+    // test 관련 ExceptionHandler
     @ExceptionHandler(value = {TestException.class})
     protected ResponseEntity<ErrorResponse> handleTestException(TestException e) {
         log.error("TestException", e);
-        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getErrorCode(), e.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getHttpStatus().toString(), e.getErrorCode().getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(errorResponse);
+    }
+    // Authentication 관련 ExceptionHandler
+    @ExceptionHandler(value = {AuthenticationException.class})
+    protected ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        log.error("AuthenticationException", e);
+        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getHttpStatus().toString(), e.getErrorCode().getErrorCode(), e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(errorResponse);
+    }
+    // 지원하지 않는 Http Method 관련 ExceptionHandler
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException", e);
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED.toString(), "H-000", e.getMessage());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
+    }
+    // 그 외 Exception
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Exception", e);
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "E-000", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
